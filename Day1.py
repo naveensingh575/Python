51d9911d77d676ac84cefcd066ba5e87ae0cf5f9
Naveen KUmar <naveenofficial210@gmail.com>
2025-03-31 16:07:36 +0530

    day1 and day2

---
+++ Day1.py
@@ -0,0 +1,4 @@
+city = input("You live in which city? \n")
+pet_name = input("What is the name of your pet? \n")
+band_name = pet_name + city
+print(f"Your band name is {band_name}")
---
+++ Untitled-1.py
@@ -0,0 +1,3 @@
+print("My Name" + " is Naveen" + " Kumar")
+print("Namste india" + "\nI am here" + "\n")
+print("My things are fine")
